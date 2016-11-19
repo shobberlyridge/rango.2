43b55c53aa7b588a455503f2b00eae6d3ed6af6b
shobberlyridge <edward@albany.me.uk>
2016-11-19 16:08:20 +0000

    every which

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -7,6 +7,9 @@ from django.db import models
 class Category(models.Model):
 	name = models.CharField(max_length=128, unique=True)
 	
+	class Meta:
+		verbose_name_plural = 'Categories'
+	
 	def __unicode__(self): # For Python 2, use __unicode__ too
 		return self.name
 		
