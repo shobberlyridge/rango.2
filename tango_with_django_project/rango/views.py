09b9758f9fe38888aadefea00a7a92be1af69627
shobberlyridge <edward@albany.me.uk>
2016-12-20 19:19:27 +0000

    end

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -30,7 +30,8 @@ def index(request):
 	# Place the list in our context_dict dictionary
 	# that will be passed to the template engine.
 	category_list = Category.objects.order_by('-likes')[:5]
-	context_dict = {'categories': category_list}
+	page_list = Page.objects.order_by('-views')[:5]
+	context_dict = {'categories': category_list, 'pages': page_list}	
 	# Render the response and send it back!
 	return render(request, 'rango/index.html', context_dict)
 
@@ -45,3 +46,5 @@ def show_category(request, category_name_slug):
 		context_dict['category'] = None
 		context_dict['pages'] = None
 	return render(request, 'rango/category.html', context_dict)
+	
+
